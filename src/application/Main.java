12d328a22a0ec32d9d627c3358802c191404b426
Guilherme Franklin <gui.franklin17@gmail.com>
2026-03-31 09:56:47 -0300

    Dao interfaces

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,7 +1,7 @@
 package application;
 
-import entities.model.Department;
-import entities.model.Seller;
+import model.entities.Department;
+import model.entities.Seller;
 
 import java.util.Date;
 
